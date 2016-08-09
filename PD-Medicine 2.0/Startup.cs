a0fdad4629a0268e5bb63918c983d972b9a30091
Georgi Dobrishinov <dobrishinov@gmail.com>
2016-08-09 10:09:03 +0300

    first commit, init project

---
+++ PD-Medicine 2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PD_Medicine_2._0.Startup))]
+namespace PD_Medicine_2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
